79f9d6b138bdcd849fb7f16d79a9465b2decc775
Kousei-Uchu <kouseicomms@gmail.com>
2025-10-24 21:10:56 +1100

    Oops forgot to add them to index.d.ts!

--- index.d.ts
+++ index.d.ts
@@ -9,3 +9,11 @@ export interface PrideFlagCustomProps {
 
 export declare const PrideFlagCustom: React.FC<PrideFlagCustomProps>;
 export declare const NonBinaryFlag: React.FC<PrideFlagCustomProps>;
+export declare const BisexualFlag: React.FC<PrideFlagCustomProps>;
+export declare const AsexualFlag: React.FC<PrideFlagCustomProps>;
+export declare const PansexualFlag: React.FC<PrideFlagCustomProps>;
+export declare const PolysexualFlag: React.FC<PrideFlagCustomProps>;
+export declare const AgenderFlag: React.FC<PrideFlagCustomProps>;
+export declare const PrideFlag: React.FC<PrideFlagCustomProps>;
+export declare const TransgenderFlag: React.FC<PrideFlagCustomProps>;
+export declare const GenderqueerFlag: React.FC<PrideFlagCustomProps>;
